c3953cc8c284a97bf9de5365ec548898c758d760
centaurreader <centaur@centaurreader.com>
2015-11-18 22:12:17 -0600

    add isBot helper prop

--- src/classes/event.js
+++ src/classes/event.js
@@ -2,6 +2,7 @@ function Event(data) {
   this.type = data.type;
   this.channel = data.channel;
   this.user = data.user;
+  this.isBot = data.username === "slugbot";
   this.text = data.text;
   this.ts = data.ts;
   this.team = data.team;
